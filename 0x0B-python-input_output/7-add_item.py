817ae1802e16e52ac11b0f86eab4ae52e6ddd4b6
Ajibola Junaid <jaeybee98@gmail.com>
2023-06-13 16:09:06 -0700

    A function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:
    Prototype: def class_to_json(obj):
    obj is an instance of a Class
    All attributes of the obj Class are serializable: list, dictionary, string, integer and boolean
    You are not allowed to import any module.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
-Module 7-add_item
+''' script that adds all arguments to a Python list, and then save them to a file
+'''
+import json
+import sys
 
-Contains function that adds and saves to Python obj to JSON file; loads objects
-
-# run with ./7-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./9-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
-
-"""
-
-
-from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-
 try:
-    existing_content = load_from_json_file(filename)
+    py_list = load_from_json_file(filename)
 except FileNotFoundError:
-    existing_content = []
-
-save_to_json_file(existing_content + argv[1:], filename)
+    py_list = []
+finally:
+    for el in sys.argv[1:]:
+        py_list.append(str(el))
+    save_to_json_file(py_list, filename)
